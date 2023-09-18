e1bec79f8cc2ebc336143feab54a47e048e13fb3
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-09-18 14:11:01 +0300

    hw032

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -32,9 +32,9 @@ export class App extends Component {
     const nextPage = this.state.galleryPage;
 
     if (prevQuery !== nextQuery) {
-      
+      this.setState({ galleryPage: 1, galleryItems: [], isButtonShow: false });
       if (nextPage === 1) {
-        
+        this.fetchGalleryItems(nextQuery, nextPage);
       }
     } else if (prevPage !== nextPage) {
       this.fetchGalleryItems(nextQuery, nextPage);
@@ -58,14 +58,11 @@ export class App extends Component {
           largeImageURL,
         })
       );
-      
+      const currentData = [...this.state.galleryItems, ...newData];
 
-      
-      this.steState(prev =>({
-          galleryItems: [prevState.galleryItems, ...newData],
-          loadMore: this.state.page < Math.ceil(totalHits / 12 )
-         }))
-      });
+      this.setState(prevState => ({
+        galleryItems: [...prevState.galleryItems, ...newData],
+      }));
 
       if (!data.totalHits) {
         this.setState({ loading: false, error: true });
@@ -73,3 +70,53 @@ export class App extends Component {
           'Sorry, there are no images matching your search query. Please try again.'
         );
       }
+
+      if (currentData.length >= data.totalHits) {
+        this.setState({
+          loading: false,
+          isButtonShow: false,
+          error: false,
+        });
+        return;
+      }
+
+      if (nextPage === 1) {
+        toast.success(`Hooray! We found ${postApiService.hits} images.`);
+      }
+
+      this.setState({
+        loading: false,
+        isButtonShow: true,
+        error: false,
+      });
+    });
+  };
+
+  handleFormSubmit = searchQuery => {
+    this.setState({ searchQuery });
+  };
+
+  onLoadMore = () => {
+    this.setState(prevState => ({
+      galleryPage: prevState.galleryPage + 1,
+    }));
+  };
+
+  render() {
+    const { galleryItems, loading, isButtonShow, error } = this.state;
+
+    return (
+      <AppContent>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+
+        {error && <h2>Please, enter search word!</h2>}
+        {!error && <ImageGallery galleryItems={galleryItems} />}
+        {loading && <Loader />}
+        {isButtonShow && <Button onClick={this.onLoadMore} />}
+
+        {/* Additions  */}
+        <ToastContainer autoClose={3000} theme="dark" />
+      </AppContent>
+    );
+  }
+}
